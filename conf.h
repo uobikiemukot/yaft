eb3901a759c2bb6667c8a8958a843f6470f67ec2
uobikiemukot <mxs@m2.gyao.ne.jp>
2012-06-20 01:29:10 +0900

    * デフォルトのフォントを変更

--- conf.h
+++ conf.h
@@ -1,9 +1,9 @@
 /* wallpaper */
-static char *wall_path = "~/.yaft/karmic-gray.ppm";
-//static char *wall_path = NULL;
+//static char *wall_path = "./img/karmic-gray.ppm";
+static char *wall_path = NULL;
 
 /* font path */
-static char *font_path = "~/.yaft/efont.yaft";
+static char *font_path = "./fonts/shnm.yaft";
 
 /* framubuffer device */
 static char *fb_path = "/dev/fb0";
@@ -24,20 +24,20 @@ enum {
 
 /* terminal setting */
 enum {
-	DUMP = true,
-	DEBUG = true,
+	DUMP = false,
+	DEBUG = false,
 	LAZYDRAW = true,
 	/*
+	*/
 	OFFSET_X = 0,
 	OFFSET_Y = 0,
 	TERM_WIDTH = 1280,
 	TERM_HEIGHT = 1024,
-	*/
+	/*
 	OFFSET_X = 32,
 	OFFSET_Y = 32,
 	TERM_WIDTH = 640, // 8 * 80
 	TERM_HEIGHT = 384, // 16 * 24
-	/*
 	*/
 	TABSTOP = 8,
 	INTERVAL = 1000000, /* polling interval(usec) */
