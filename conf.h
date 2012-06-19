06e6d82654b8066c90a414ddd0aa9153278e3e6c
uobikiemukot <mxs@m2.gyao.ne.jp>
2012-06-19 13:50:15 +0900

    1340081415

--- conf.h
+++ conf.h
@@ -1,9 +1,8 @@
 /* wallpaper */
-//static char *wall_path = "./img/karmic-gray.ppm";
 static char *wall_path = NULL;
 
 /* font path */
-static char *font_path = "./fonts/test.yaft";
+static char *font_path = "~/.fonts/efont.yaft";
 
 /* framubuffer device */
 static char *fb_path = "/dev/fb0";
@@ -27,19 +26,10 @@ enum {
 	DUMP = false,
 	DEBUG = false,
 	LAZYDRAW = true,
-	/*
 	OFFSET_X = 0,
 	OFFSET_Y = 0,
 	TERM_WIDTH = 1280,
 	TERM_HEIGHT = 1024,
-	*/
-	OFFSET_X = 32,
-	OFFSET_Y = 32,
-	TERM_WIDTH = 640, // 8 * 80
-	TERM_HEIGHT = 384, // 16 * 24
-	/*
-	*/
 	TABSTOP = 8,
 	INTERVAL = 1000000, /* polling interval(usec) */
-	//SCROLLBACK = 1024, /* not implemented */
 };
