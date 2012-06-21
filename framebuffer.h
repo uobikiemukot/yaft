2b917948d17549a7bf532f7ee4f3ea7a79ef5f78
uobikiemukot <mxs@m2.gyao.ne.jp>
2012-06-22 01:42:55 +0900

    * attr_maskに関連する修正

--- framebuffer.h
+++ framebuffer.h
@@ -1,13 +1,12 @@
-void fb_init(framebuffer *fb)
+void fb_init(framebuffer * fb)
 {
 	char *file;
-	int ret;
 	fix_info finfo;
 	var_info vinfo;
 
 	if ((file = getenv("FRAMEBUFFER")) != NULL)
 		fb->fd = eopen(file, O_RDWR);
-	else 
+	else
 		fb->fd = eopen(fb_path, O_RDWR);
 
 	eioctl(fb->fd, FBIOGET_FSCREENINFO, &finfo);
@@ -18,16 +17,18 @@ void fb_init(framebuffer *fb)
 	fb->sc_size = finfo.smem_len;
 	fb->line_length = finfo.line_length / sizeof(u32);
 
-	fb->fp = emmap(0, fb->sc_size, PROT_WRITE | PROT_READ, MAP_SHARED, fb->fd, 0);
+	fb->fp =
+		emmap(0, fb->sc_size, PROT_WRITE | PROT_READ, MAP_SHARED, fb->fd,
+			  0);
 }
 
-void fb_die(framebuffer *fb)
+void fb_die(framebuffer * fb)
 {
-	emunmap(fb->fp, fb->sc_size);
-	eclose(fb->fd);
+	munmap(fb->fp, fb->sc_size);
+	close(fb->fd);
 }
 
-void set_bitmap(terminal *term, int y, int x, int offset, u32 *src)
+void set_bitmap(terminal * term, int y, int x, int offset, u32 * src)
 {
 	int i;
 	color_pair color;
@@ -40,22 +41,24 @@ void set_bitmap(terminal *term, int y, int x, int offset, u32 *src)
 	color = cp->color;
 
 	/* attribute */
-	if ((cp->attribute & UNDERLINE) && (offset == (term->cell_size.y - 1)))
+	if ((cp->attribute & attr_mask[UNDERLINE])
+		&& (offset == (term->cell_size.y - 1)))
 		color.bg = color.fg;
-	if (cp->attribute & REVERSE) {
+	if (cp->attribute & attr_mask[REVERSE]) {
 		tmp = color.fg;
 		color.fg = color.bg;
 		color.bg = tmp;
 	}
 
-	switch(cp->wide) {
+	switch (cp->wide) {
 	case HALF:
 		for (i = 1; i <= gp->size.x; i++) {
 			if ((gp->bitmap[offset] >> (gp->size.x - i)) & 0x01)
 				*(src + i - 1) = color.fg;
 			else if (term->wall != NULL && color.bg == DEFAULT_BG)
-				*(src + i - 1) = *(term->wall + x * term->cell_size.x + (i - 1)
-					+ (y * term->cell_size.y + offset) * term->width);
+				*(src + i - 1) =
+					*(term->wall + x * term->cell_size.x + (i - 1)
+					  + (y * term->cell_size.y + offset) * term->width);
 			else
 				*(src + i - 1) = color.bg;
 		}
@@ -63,20 +66,25 @@ void set_bitmap(terminal *term, int y, int x, int offset, u32 *src)
 	case WIDE:
 		for (i = 1; i <= gp->size.x; i++) {
 			if (i <= term->cell_size.x) {
-				if ((gp->bitmap[offset * 2] >> (term->cell_size.x - i)) & 0x01)
+				if ((gp->
+					 bitmap[offset * 2] >> (term->cell_size.x - i)) & 0x01)
 					*(src + i - 1) = color.fg;
 				else if (term->wall != NULL && color.bg == DEFAULT_BG)
-					*(src + i - 1) = *(term->wall + x * term->cell_size.x + (i - 1)
-						+ (y * term->cell_size.y + offset) * term->width);
+					*(src + i - 1) =
+						*(term->wall + x * term->cell_size.x + (i - 1)
+						  + (y * term->cell_size.y +
+							 offset) * term->width);
 				else
 					*(src + i - 1) = color.bg;
-			}
-			else {
-				if ((gp->bitmap[offset * 2 + 1] >> (gp->size.x - i)) & 0x01)
+			} else {
+				if ((gp->
+					 bitmap[offset * 2 + 1] >> (gp->size.x - i)) & 0x01)
 					*(src + i - 1) = color.fg;
 				else if (term->wall != NULL && color.bg == DEFAULT_BG)
-					*(src + i - 1) = *(term->wall + x * term->cell_size.x + (i - 1)
-						+ (y * term->cell_size.y + offset) * term->width);
+					*(src + i - 1) =
+						*(term->wall + x * term->cell_size.x + (i - 1)
+						  + (y * term->cell_size.y +
+							 offset) * term->width);
 				else
 					*(src + i - 1) = color.bg;
 			}
@@ -88,13 +96,14 @@ void set_bitmap(terminal *term, int y, int x, int offset, u32 *src)
 	}
 }
 
-void draw_line(framebuffer *fb, terminal *term, int line)
+void draw_line(framebuffer * fb, terminal * term, int line)
 {
 	int i, j, size;
-	cell *cp;
-	u32 *p, *src, *dst, save_bg;
+	u32 *p, *src, *dst;
 
-	p = fb->fp + term->offset.x + (term->offset.y + line * term->cell_size.y) * fb->line_length;
+	p = fb->fp + term->offset.x + (term->offset.y +
+								   line * term->cell_size.y) *
+		fb->line_length;
 	size = term->width * sizeof(u32);
 	src = emalloc(size);
 
@@ -109,28 +118,36 @@ void draw_line(framebuffer *fb, terminal *term, int line)
 	free(src);
 }
 
-void draw_curs(framebuffer *fb, terminal *term)
+void draw_curs(framebuffer * fb, terminal * term)
 {
 	int i, size;
-	color_pair store;
+	color_pair store_color;
+	pair store_curs;
+	u8 store_attr;
 	cell *cp;
 	u32 *p, *src, *dst;
 
+	store_curs = term->cursor;
+	if (term->cells[term->cursor.x + term->cursor.y * term->cols].wide ==
+		NEXT_TO_WIDE)
+		term->cursor.x--;
+
 	p = fb->fp + term->offset.x + term->cursor.x * term->cell_size.x
-		+ (term->offset.y + term->cursor.y * term->cell_size.y) * fb->line_length;
+		+ (term->offset.y +
+		   term->cursor.y * term->cell_size.y) * fb->line_length;
 	cp = &term->cells[term->cursor.x + term->cursor.y * term->cols];
 
-	store = cp->color;
-	cp->color.fg = DEFAULT_FG;
+	store_color = cp->color;
+	cp->color.fg = DEFAULT_BG;
 	cp->color.bg = CURSOR_COLOR;
 
-	if (cp->wide == NEXT_TO_WIDE)
-		return;
+	store_attr = cp->attribute;
+	cp->attribute = RESET;
 
 	size = (cp->wide == WIDE) ?
-		size = 2 * term->cell_size.x * sizeof(u32):
+		2 * term->cell_size.x * sizeof(u32) :
 		term->cell_size.x * sizeof(u32);
-		
+
 	src = emalloc(size);
 
 	for (i = 0; i < term->cell_size.y; i++) {
@@ -138,16 +155,17 @@ void draw_curs(framebuffer *fb, terminal *term)
 		dst = p + i * fb->line_length;
 		memcpy(dst, src, size);
 	}
-	cp->color = store;
+	cp->color = store_color;
+	cp->attribute = store_attr;
+	term->cursor = store_curs;
 	term->line_dirty[term->cursor.y] = true;
 
 	free(src);
 }
 
-void refresh(framebuffer *fb, terminal *term)
+void refresh(framebuffer * fb, terminal * term)
 {
-	int i, j;
-	cell *cp;
+	int i;
 
 	for (i = 0; i < term->lines; i++) {
 		if (term->line_dirty[i])
