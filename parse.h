0dca13b205604d4debc7745dc6bafdcc049baecc
uobikiemukot <uobikiemukot@gmail.com>
2012-06-22 01:41:44 +0900

    * OSCを廃止したので関数が減った

--- parse.h
+++ parse.h
@@ -1,4 +1,4 @@
-void reset_func(void (**func)(terminal *term, void *arg), int num)
+void reset_func(void (**func) (terminal * term, void *arg), int num)
 {
 	int i;
 
@@ -6,20 +6,20 @@ void reset_func(void (**func)(terminal *term, void *arg), int num)
 		func[i] = ignore;
 }
 
-void load_ctrl_func(void (**func)(terminal *term, void *arg), int num)
+void load_ctrl_func(void (**func) (terminal * term, void *arg), int num)
 {
 	reset_func(func, num);
 
-	func[0x08] = bs;        /* BS  */
-	func[0x09] = tab;       /* HT  */
-	func[0x0A] = nl;        /* LF  */
-	func[0x0B] = nl;        /* VT  */
-	func[0x0C] = nl;        /* FF  */
-	func[0x0D] = cr;        /* CR  */
-	func[0x1B] = enter_esc; /* ESC */
+	func[0x08] = bs;			/* BS  */
+	func[0x09] = tab;			/* HT  */
+	func[0x0A] = nl;			/* LF  */
+	func[0x0B] = nl;			/* VT  */
+	func[0x0C] = nl;			/* FF  */
+	func[0x0D] = cr;			/* CR  */
+	func[0x1B] = enter_esc;		/* ESC */
 }
 
-void load_esc_func(void (**func)(terminal *term, void *arg), int num)
+void load_esc_func(void (**func) (terminal * term, void *arg), int num)
 {
 	reset_func(func, num);
 
@@ -35,7 +35,7 @@ void load_esc_func(void (**func)(terminal *term, void *arg), int num)
 	func['c'] = ris;
 }
 
-void load_csi_func(void (**func)(terminal *term, void *arg), int num)
+void load_csi_func(void (**func) (terminal * term, void *arg), int num)
 {
 	reset_func(func, num);
 
@@ -70,75 +70,16 @@ void load_csi_func(void (**func)(terminal *term, void *arg), int num)
 	func['`'] = curs_col;
 }
 
-void load_osc_func(void (**func)(terminal *term, void *arg), int num)
-{
-	reset_func(func, num);
-
-	func[4] = set_palette;
-	func[104] = reset_palette;
-}
-
 /* need to rewrite this function */
-int parse_osc(terminal *term, int *argc, char **argv)
+u8 parse_csi(terminal * term, int *argc, char **argv)
 {
-	int i, length;
+	int length;
 	u8 c, *cp;
 	escape *ep;
 
-	if (DEBUG)
-		fprintf(stderr, "parsing osc: %s\n", term->esc.buf);
-
 	ep = &term->esc;
 
-	length = strlen(ep->buf);
-	c = ep->buf[length - 1];
-
-	ep->buf[length - 1] = '\0';
-	if (c == 0x5C)
-		ep->buf[length - 2] = '\0';
-
-	cp = ep->buf;
-	while (cp != &ep->buf[length - 1]) {
-		if (*cp == ';' || *cp == ':' || *cp == '/' || *cp == '#')
-			*cp = '\0';
-		cp++;
-	}
-
-	cp = ep->buf;
-start:
-	if (isalnum(*cp)) {
-		argv[*argc] = cp;
-		*argc += 1;
-	}
-
-	while (isalnum(*cp))
-		cp++;
-
-	while (!isalnum(*cp) && cp != &ep->buf[length - 1])
-		cp++;
-
-	if (cp != &ep->buf[length - 1])
-		goto start;
-
-	if (*argc > 0)
-		return atoi(argv[0]);
-	else
-		return 0;
-}
-
-/* need to rewrite this function */
-u8 parse_csi(terminal *term, int *argc, char **argv)
-{
-	int i, length;
-	u8 c, *cp;
-	escape *ep;
-
-	if (DEBUG)
-		fprintf(stderr, "parsing osc: %s\n", term->esc.buf);
-
-	ep = &term->esc;
-
-	length = strlen(ep->buf);
+	length = strlen((char *) ep->buf);
 	c = ep->buf[length - 1];
 	ep->buf[length - 1] = '\0';
 
@@ -150,9 +91,9 @@ u8 parse_csi(terminal *term, int *argc, char **argv)
 	}
 
 	cp = ep->buf;
-start:
+  start:
 	if (isdigit(*cp)) {
-		argv[*argc] = cp;
+		argv[*argc] = (char *) cp;
 		*argc += 1;
 	}
 
@@ -160,7 +101,7 @@ start:
 		cp++;
 
 	while (!isdigit(*cp) && cp != &ep->buf[length - 1])
-			cp++;
+		cp++;
 
 	if (cp != &ep->buf[length - 1])
 		goto start;
@@ -169,32 +110,32 @@ start:
 }
 
 static char *ctrl_char[] = {
-	"NUL","SOH","STX","ETX","EOT","ENQ","ACK","BEL",
-	"BS ","HT ","LF ","VT ","FF ","CR ","SO ","SI ",
-	"DLE","DC1","DC2","DC3","DC4","NAK","SYN","ETB",
-	"CAN","EM ","SUB","ESC","FS ","GS ","RS ","US ",
+	"NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
+	"BS ", "HT ", "LF ", "VT ", "FF ", "CR ", "SO ", "SI ",
+	"DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
+	"CAN", "EM ", "SUB", "ESC", "FS ", "GS ", "RS ", "US ",
 };
 
-void control_character(terminal *term, u8 ch)
+void control_character(terminal * term, u8 ch)
 {
 	if (DEBUG)
-		fprintf(stderr, "control char: %s\n", ctrl_char[ch]);
+		fprintf(stderr, "ctl: %s\n", ctrl_char[ch]);
 
-	ctrl_func[ch](term, NULL);
+	ctrl_func[ch] (term, NULL);
 }
 
-void esc_sequence(terminal *term, u8 ch)
+void esc_sequence(terminal * term, u8 ch)
 {
 	if (DEBUG)
-		fprintf(stderr, "escape sequence char: ESC %c\n", ch);
+		fprintf(stderr, "esc: ESC %c\n", ch);
 
-	esc_func[ch](term, NULL);
+	esc_func[ch] (term, NULL);
 
 	if (ch != '[' && ch != ']')
 		term->esc.state = RESET;
 }
 
-void reset_parm(parm_t *pt)
+void reset_parm(parm_t * pt)
 {
 	int i;
 
@@ -203,57 +144,46 @@ void reset_parm(parm_t *pt)
 		pt->argv[i] = NULL;
 }
 
-void csi_sequence(terminal *term)
+void csi_sequence(terminal * term)
 {
- 	u8 c;
+	u8 c;
 	parm_t parm;
 
 	if (DEBUG)
-		fprintf(stderr, "escape sequence char: CSI %s\n", term->esc.buf);
+		fprintf(stderr, "csi: CSI %s\n", term->esc.buf);
 
 	reset_parm(&parm);
 	c = parse_csi(term, &parm.argc, parm.argv);
 
-	csi_func[c](term, &parm);
+	csi_func[c] (term, &parm);
 	reset_esc(term);
 }
 
-void osc_sequence(terminal *term)
+void osc_sequence(terminal * term)
 {
-	int num;
-	parm_t parm;
-
 	if (DEBUG)
-		fprintf(stderr, "escape sequence char: OSC %s\n", term->esc.buf);
-
-	reset_parm(&parm);
-	num = parse_osc(term, &parm.argc, parm.argv);
-
-	osc_func[num](term, &parm);
+		fprintf(stderr, "osc: OSC %s\n", term->esc.buf);
 	reset_esc(term);
 }
 
-void utf8_character(terminal *term, u8 ch)
+void utf8_character(terminal * term, u8 ch)
 {
 	if (0xC2 <= ch && ch <= 0xDF) {
 		term->ucs.length = 1;
 		term->ucs.count = 0;
 		term->ucs.code = ch & 0x1F;
 		return;
-	}
-	else if (0xE0 <= ch && ch <= 0xEF) {
+	} else if (0xE0 <= ch && ch <= 0xEF) {
 		term->ucs.length = 2;
 		term->ucs.count = 0;
 		term->ucs.code = ch & 0x0F;
 		return;
-	}
-	else if (0xF0 <= ch && ch <= 0xF4) {
+	} else if (0xF0 <= ch && ch <= 0xF4) {
 		term->ucs.length = 3;
 		term->ucs.count = 0;
 		term->ucs.code = ch & 0x07;
 		return;
-	}
-	else if (0x80 <= ch && ch <= 0xBF) {
+	} else if (0x80 <= ch && ch <= 0xBF) {
 		if (term->ucs.length == 0) {
 			reset_ucs(term);
 			return;
@@ -261,8 +191,7 @@ void utf8_character(terminal *term, u8 ch)
 		term->ucs.code <<= 6;
 		term->ucs.code += ch & 0x3F;
 		term->ucs.count++;
-	}
-	else { /* 0xC0 - 0xC1, 0xF5 - 0xFF: illegal byte */
+	} else {					/* 0xC0 - 0xC1, 0xF5 - 0xFF: illegal byte */
 		reset_ucs(term);
 		return;
 	}
@@ -275,7 +204,7 @@ void utf8_character(terminal *term, u8 ch)
 	}
 }
 
-void parse(terminal *term, u8 *buf, int size)
+void parse(terminal * term, u8 * buf, int size)
 {
 	u8 ch;
 	int i;
@@ -289,21 +218,17 @@ void parse(terminal *term, u8 *buf, int size)
 				if (DEBUG)
 					fprintf(stderr, "ascii: %c\n", ch);
 				addch(term, ch);
-			}
-			else if (ch <= 0xFF)
+			} else if (ch <= 0xFF)
 				utf8_character(term, ch);
-		}
-		else if (term->esc.state == ESC) {
+		} else if (term->esc.state == ESC) {
 			if (ch <= 0x7F)
 				esc_sequence(term, ch);
 			else
 				term->esc.state = RESET;
-		}
-		else if (term->esc.state == CSI) {
+		} else if (term->esc.state == CSI) {
 			if (push_esc(term, ch))
 				csi_sequence(term);
-		}
-		else if (term->esc.state == OSC) {
+		} else if (term->esc.state == OSC) {
 			if (push_esc(term, ch))
 				osc_sequence(term);
 		}
