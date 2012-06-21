06d1ae97d914d6059ed19689d68b1ff8745539fe
uobikiemukot <mxs@m2.gyao.ne.jp>
2012-06-21 12:46:20 +0900

    * terminfoを使った256色のチェックプログラム

---
+++ misc/256color.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <term.h>
+
+enum {
+	ANSI = 8,
+	ANSI_BOLD = 16,
+	COLORCUBE = 232,
+	COLORS = 256,
+};
+
+void fatal(char *message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+void print_color_block(int color)
+{
+	char *str;
+
+	str = tparm(set_a_background, color);
+	putp(str);
+	printf("  ");
+}
+
+void reset_color()
+{
+	putp(exit_attribute_mode);
+}
+
+int main()
+{
+	int i, j, k, status, color;
+
+	setupterm(NULL, STDOUT_FILENO, &status);
+	if (status != 1)
+		fatal("Error in setupterm");
+
+	if (set_a_foreground == NULL
+		|| exit_attribute_mode == NULL)
+		return 1;
+
+	printf("System colors:\n");
+	for (i = 0; i < ANSI; i++)
+		print_color_block(i);
+	reset_color();
+	printf("\n");
+
+	for (i = ANSI; i < ANSI_BOLD; i++)
+		print_color_block(i);
+	reset_color();
+	printf("\n\n");
+
+	printf("Color cube, 6x6x6:\n");
+	for (i = 0; i < 6; i++) {
+		for (j = 0; j < 6; j++) {
+			for (k = 0; k < 6; k++) {
+				color = 16 + (j * 36) + (i * 6) + k;
+				print_color_block(color);
+			}
+			putp(exit_attribute_mode);
+			printf(" ");
+		}
+		printf("\n");
+	}
+	reset_color();
+	printf("\n");
+
+	printf("Grayscale ramp:\n");
+	for (i = COLORCUBE; i < COLORS; i++)
+		print_color_block(i);
+	reset_color();
+	printf("\n");
+
+	return 0;
+}
