39c8838bdc7c790e114b108d59093f957c3f4d1a
uobikiemukot <mxs@m2.gyao.ne.jp>
2012-06-24 22:46:11 +0900

    * ~をパスとして使えなくした * フォントの形式が変わったので読み込み部分を修正

--- load.h
+++ load.h
@@ -1,39 +1,13 @@
-void cat_home(char *dst, char *src, int size)
+/* See LICENSE for licence details. */
+void load_fonts(glyph_t **fonts, char *path)
 {
-	char *home;
-
-	if (src == NULL) {
-		fprintf(stderr, "font_path == NULL\n");
-		exit(EXIT_FAILURE);
-	}
-
-	memset(dst, '\0', size);
-
-	if (DEBUG)
-		fprintf(stderr, "src:%s dst:%s size:%d\n", src, dst, size);
-
-	if (src[0] == '~') {
-		if ((home = getenv("HOME")) != NULL) {
-			strncpy(dst, home, size - 1);
-			strncat(dst, src + 1, size - strlen(dst) - 1);
-			if (DEBUG)
-				fprintf(stderr, "src:%s dst:%s size:%d\n", src, dst, size);
-		} else
-			strncpy(dst, src + 1, size - 1);
-	} else
-		strncpy(dst, src, size - 1);
-}
-
-void load_fonts(glyph_t ** fonts, char *path)
-{
-	int i, count = 0, size = 0, state = 0;
-	char buf[BUFSIZE], home[BUFSIZE], *endp;
+	int i, count = 0, state = 0;
+	char buf[BUFSIZE], *endp;
 	FILE *fp;
 	u16 code = DEFAULT_CHAR;
 	glyph_t *gp = NULL;
 
-	cat_home(home, path, BUFSIZE);
-	fp = efopen(home, "r");
+	fp = efopen(path, "r");
 
 	for (i = 0; i < UCS_CHARS; i++)
 		fonts[i] = NULL;
@@ -47,13 +21,12 @@ void load_fonts(glyph_t ** fonts, char *path)
 			break;
 		case 1:
 			sscanf(buf, "%d %d", &gp->size.x, &gp->size.y);
-			size = gp->size.x * gp->size.y / BITS_PER_BYTE;
-			gp->bitmap = (u8 *) emalloc(size);
+			gp->bitmap = (u32 *) emalloc(gp->size.y * sizeof(u32));
 			state = 2;
 			break;
 		case 2:
 			gp->bitmap[count++] = strtol(buf, &endp, 16);
-			if (count >= size) {
+			if (count >= gp->size.y) {
 				fonts[code] = gp;
 				state = count = 0;
 			}
@@ -63,16 +36,17 @@ void load_fonts(glyph_t ** fonts, char *path)
 		}
 	}
 
-	if (fonts[DEFAULT_CHAR] == NULL) {
-		fprintf(stderr, "fonts must have DEFAULT_CHAR(U+%.2X)\n",
-				DEFAULT_CHAR);
+	gp = fonts[DEFAULT_CHAR];
+	if (gp == NULL || gp->size.x == 0 || gp->size.y == 0) {
+		fprintf(stderr, "DEFAULT_CHAR(U+%.2X) not found or invalid cell size x:%d y:%d\n",
+				DEFAULT_CHAR, gp->size.x, gp->size.y);
 		exit(EXIT_FAILURE);
 	}
 
 	fclose(fp);
 }
 
-u32 *load_wallpaper(framebuffer * fb, int width, int height)
+u32 *load_wallpaper(framebuffer *fb, int width, int height)
 {
 	int i, j, count = 0;
 	u32 *ptr;
