706fdd69c44ea6effcec4952716a1ea1edb13439
uobikiemukot <uobikiemukot@gmail.com>
2012-06-24 22:41:02 +0900

    * 制御文字を定義した．その他色々?

--- common.h
+++ common.h
@@ -1,18 +1,20 @@
+/* See LICENSE for licence details. */
 #define _XOPEN_SOURCE 600
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <linux/fb.h>
 #include <sys/mman.h>
 #include <sys/select.h>
+#include <ctype.h>
 #include <string.h>
 #include <signal.h>
+#include <termios.h>
 #include <pty.h>
-#include <ctype.h>
+#include <math.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -40,17 +42,21 @@ typedef struct parm_t parm_t;
 #include "color.h"				/* 256color definition */
 #include "conf.h"				/* user configuration */
 
+enum char_code {
+	BEL = 0x07, CAN = 0x18, SUB = 0x1A, ESC = 0x1B,
+	SPACE = 0x20, US = 0x1F, BACKSLASH = 0x5C, DEL = 0x7F,
+};
+
 enum {
 	/* misc */
 	BITS_PER_BYTE = 8,
+	MASK_7BIT = 0x7F,
 	BUFSIZE = 1024,				/* read, esc, various buffer size */
-	ESC_PARAMS = 64,			/* max parameters of csi/osc sequence */
+	ESC_PARAMS = 16,			/* max parameters of csi/osc sequence */
 	COLORS = 256,				/* num of color */
 	UCS_CHARS = 0x10000,		/* number of UCS2 glyph */
 	CTRL_CHARS = 0x20,			/* number of control character */
 	ESC_CHARS = 0x80,			/* number of final character of escape sequence */
-	SPACE = 0x20,
-	DEL = 0x7F,
 	DEFAULT_CHAR = SPACE,		/* erase char, and used for cell_size: SPACE */
 	RESET = 0x00,				/* reset for char_attr, term_mode, esc_state */
 	BRIGHT_INC = 8,				/* incrment value: from dark color to light color */
@@ -59,47 +65,43 @@ enum {
 enum width_flag {
 	NEXT_TO_WIDE = 0,
 	HALF,
-	WIDE,
+	WIDE
 };
 
 enum char_attr {
-	BOLD = 1,					/* only affect for ascii */
+	BOLD = 1,					/* bright foreground */
 	UNDERLINE = 4,
-	BLINK = 5,
+	BLINK = 5,					/* bright background */
 	REVERSE = 7,
 };
 
-static u8 attr_mask[] = {
-	0x00,						/* 0: not used */
-	0x01,						/* 1: bold */
-	0x00,						/* 2: not used */
-	0x00,						/* 3: not used */
-	0x02,						/* 4: underline */
-	0x04,						/* 5: blink */
-	0x00,						/* 6: not used */
-	0x08,						/* 7: reverse */
+static const u8 attr_mask[] = {
+	0x00, 0x01, 0x00, 0x00,		/* 0:none      1:bold  2:none 3:none */
+	0x02, 0x04, 0x00, 0x08,		/* 4:underline 5:blink 6:none 7:reverse */
 };
 
 enum term_mode {
 	ORIGIN = 0x01,				/* origin mode: DECOM */
 	CURSOR = 0x02,				/* cursor visible: DECTECM */
-	AMRIGHT = 0x04,
+	AMRIGHT = 0x04,				/* auto wrap: DECAWM */
 };
 
 enum esc_state {
-	ESC = 1,					/* 0x1B, \033, ESC */
-	CSI,						/* ESC [ */
-	OSC,						/* ESC ] */
+	STATE_ESC = 1,				/* 0x1B, \033, ESC */
+	STATE_CSI,					/* ESC [ */
+	STATE_OSC,					/* ESC ] */
 };
 
 struct pair {
 	int x, y;
 };
+
 struct margin {
 	int top, bottom;
 };
+
 struct color_pair {
-	u32 fg, bg;
+	int fg, bg;
 };
 
 struct framebuffer {
@@ -130,12 +132,10 @@ struct uchar {
 
 struct glyph_t {
 	pair size;
-	u8 *bitmap;
+	u32 *bitmap;
 };
 
-struct state {
-	/* saves cursor position, character attribute
-	   and origin mode selection */
+struct state {					/* for save/restore state */
 	int mode;
 	pair cursor;
 	u8 attribute;
