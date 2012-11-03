0c09cfa106629108fde9a68e2666b85608d22f76
uobikiemukot <uobikiemukot@gmail.com>
2012-11-04 01:14:47 +0900

    * FreeBSD版を追加

---
+++ freebsd/conf.h
@@ -0,0 +1,37 @@
+/* See LICENSE for licence details. */
+/* font path */
+char *font_path[] = {
+	"/usr/share/yaft/fonts/milkjf-jis0208.yaft",
+	"/usr/share/yaft/fonts/milkjf-jis0201.yaft",
+	"/usr/share/yaft/fonts/milkjf-iso8859.yaft",
+	"/usr/share/yaft/fonts/ambiguous-half.yaft",
+	NULL,
+};
+
+/* glyph alias file */
+char *glyph_alias = "/usr/share/yaft/alias/ambiguous-half.alias";
+
+/* framubuffer device */
+char *fb_path = "/dev/ttyv0";
+
+/* shell */
+char *shell_cmd = "/bin/bash";
+
+/* TERM value */
+char *term_name = "TERM=yaft-256color";
+
+/* color: index number of color_palette[] in color.h */
+enum {
+	DEFAULT_FG = 7,
+	DEFAULT_BG = 0,
+	CURSOR_COLOR = 2,
+};
+
+/* misc */
+enum {
+	WIDTH = 1024,
+	HEIGHT = 768,
+	DEPTH = 16,
+	DEBUG = false,
+	TABSTOP = 8,
+};
