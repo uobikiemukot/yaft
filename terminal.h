1ab6feb6513b3fb8a579acd09808197952d092e3
uobikiemukot <uobikiemukot@gmail.com>
2014-06-14 14:20:00 +0900

    add functions for sixel

--- terminal.h
+++ terminal.h
@@ -42,7 +42,7 @@ void swap_color(struct color_pair *cp)
 	cp->bg = tmp;
 }
 
-int set_cell(struct terminal *term, int y, int x, const struct static_glyph_t *gp)
+int set_cell(struct terminal *term, int y, int x, const struct glyph_t *gp)
 {
 	struct cell nc, *cp;
 
@@ -106,6 +106,48 @@ void scroll(struct terminal *term, int from, int to, int offset)
 	}
 }
 
+void reset_sixel(struct sixel_canvas_t *sc)
+{
+	int i;
+
+	sc->data        = NULL;
+	sc->line_length = 0;
+	sc->ratio       = 1;
+	sc->width       = sc->height     = 0;
+	sc->point.x     = sc->point.y    = 0;
+	sc->ref_cell.x  = sc->ref_cell.y = 0;
+	sc->color_index = 0;
+
+	for (i = 0; i < COLORS; i++)
+		sc->color_table[i] = color_list[15];
+}
+
+void scroll_sixel(struct terminal *term, int from, int to, int offset)
+{
+	/* TODO: check sixel scrolling mode */
+	int i;
+	struct sixel_canvas_t *sc;
+
+	if (offset == 0 || from >= to)
+		return;
+
+	if (!(term->mode & MODE_SIXSCR))
+		return;
+
+	for (i = 0; i < MAX_SIXEL_CANVAS; i++) {
+		sc = &term->sixel_canvas[i];
+
+		if (from <= sc->ref_cell.y && sc->ref_cell.y <= to)
+			sc->ref_cell.y -= offset;
+
+		if (sc->ref_cell.y < from || sc->ref_cell.y > to) {
+			if (sc->data != NULL)
+				free(sc->data);
+			reset_sixel(sc);
+		}
+	}
+}
+
 /* relative movement: cause scrolling */
 void move_cursor(struct terminal *term, int y_offset, int x_offset)
 {
@@ -121,7 +163,7 @@ void move_cursor(struct terminal *term, int y_offset, int x_offset)
 		x = 0;
 	else if (x >= term->cols) {
 		if (term->mode & MODE_AMRIGHT)
-			term->wrap = true;
+			term->wrap_occured = true;
 		x = term->cols - 1;
 	}
 	term->cursor.x = x;
@@ -132,10 +174,12 @@ void move_cursor(struct terminal *term, int y_offset, int x_offset)
 	if (term->cursor.y == top && y_offset < 0) {
 		y = top;
 		scroll(term, top, bottom, y_offset);
+		scroll_sixel(term, top, bottom, y_offset);
 	}
 	else if (term->cursor.y == bottom && y_offset > 0) {
 		y = bottom;
 		scroll(term, top, bottom, y_offset);
+		scroll_sixel(term, top, bottom, y_offset);
 	}
 	term->cursor.y = y;
 }
@@ -162,10 +206,34 @@ void set_cursor(struct terminal *term, int y, int x)
 	term->cursor.y = y;
 }
 
+const struct glyph_t *drcsch(struct terminal *term, uint32_t code)
+{
+	/* DRCSMMv1
+		ESC ( SP <\xXX> <\xYY> ESC ( B
+		<===> U+10XXYY ( 0x40 <= 0xXX <=0x7E, 0x20 <= 0xYY <= 0x7F )
+	*/
+	int ku, ten;
+
+	ku  = (0xFF00 & code) >> 8;
+	ten = 0xFF & code;
+
+	if (DEBUG)
+		fprintf(stderr, "drcs ku:0x%.2X ten:0x%.2X\n", ku, ten);
+
+	if ((0x40 <= ku && ku <= 0x7E)
+		&& (0x20 <= ten && ten <= 0x7F)
+		&& (term->drcs[ku - 0x40] != NULL))
+		return &term->drcs[ku - 0x40][ten - 0x20]; /* sub each offset */
+	else {
+		fprintf(stderr, "drcs char not found\n");
+		return term->glyph_map[SUBSTITUTE_HALF];
+	}
+}
+
 void addch(struct terminal *term, uint32_t code)
 {
-	int width, drcs_ku, drcs_ten;
-	const struct static_glyph_t *gp;
+	int width;
+	const struct glyph_t *gp;
 
 	if (DEBUG)
 		fprintf(stderr, "addch: U+%.4X\n", code);
@@ -174,26 +242,8 @@ void addch(struct terminal *term, uint32_t code)
 
 	if (width <= 0) /* zero width */
 		return;
-	else if (0x100000 <= code && 0x10FFFD) { /* Unicode private area: plane 16 */
-		/* DRCSMMv1
-			ESC ( SP <\xXX> <\xYY> ESC ( B
-			<===> U+10XXYY ( 0x40 <= 0xXX <=0x7E, 0x20 <= 0xYY <= 0x7F )
-		*/
-		drcs_ku = (0xFF00 & code) >> 8;
-		drcs_ten = 0xFF & code;
-
-		if (DEBUG)
-			fprintf(stderr, "ku:0x%.2X ten:0x%.2X\n", drcs_ku, drcs_ten);
-
-		if ((0x40 <= drcs_ku && drcs_ku <= 0x7E)
-			&& (0x20 <= drcs_ten && drcs_ten <= 0x7F)
-			&& (term->drcs[drcs_ku - 0x40].chars != NULL))
-			gp = &term->drcs[drcs_ku - 0x40].chars[drcs_ten - 0x20]; /* sub each offset */
-		else {
-			fprintf(stderr, "drcs char not found\n");
-			gp = term->glyph_map[SUBSTITUTE_HALF];
-		}
-	}
+	else if (0x100000 <= code && code <= 0x10FFFD) /* Unicode private area: plane 16 */
+		gp = drcsch(term, code);
 	else if (code >= UCS2_CHARS /* yaft support only UCS2 */
 		|| term->glyph_map[code] == NULL /* missing glyph */
 		|| term->glyph_map[code]->width != width) /* width unmatch */
@@ -201,12 +251,12 @@ void addch(struct terminal *term, uint32_t code)
 	else
 		gp = term->glyph_map[code];
 
-	if ((term->wrap && term->cursor.x == term->cols - 1) /* folding */
+	if ((term->wrap_occured && term->cursor.x == term->cols - 1) /* folding */
 		|| (gp->width == WIDE && term->cursor.x == term->cols - 1)) {
 		set_cursor(term, term->cursor.y, 0);
 		move_cursor(term, 1, 0);
 	}
-	term->wrap = false;
+	term->wrap_occured = false;
 
 	move_cursor(term, 0, set_cell(term, term->cursor.y, term->cursor.x, gp));
 }
@@ -215,55 +265,98 @@ void reset_esc(struct terminal *term)
 {
 	if (DEBUG)
 		fprintf(stderr, "*esc reset*\n");
-	memset(term->esc.buf, '\0', BUFSIZE);
+
+	if (term->esc.size > MAX_ESC_SIZE) {
+		term->esc.buf  = erealloc(term->esc.buf, MAX_ESC_SIZE);
+		term->esc.size = MAX_ESC_SIZE;
+	}
+
+	//memset(term->esc.buf, '\0', term->esc.size);
 	term->esc.bp = term->esc.buf;
 	term->esc.state = STATE_RESET;
 }
 
-bool is_string_terminator(struct esc_t *esc, uint8_t ch)
+inline bool is_string_terminator(struct esc_t *esc, uint8_t ch)
 {
 	/* OSC/DCS ST: BELL or ESC \ */
 	if (ch == BEL)
 		return true;
 
-	if (ch == BACKSLASH && strlen(esc->buf) >= 2 && *(esc->bp - 2) == ESC)
+	fprintf(stderr, "baz\n");
+
+	if (ch == BACKSLASH && (esc->bp - esc->buf) >= 2 && *(esc->bp - 2) == ESC)
 		return true;
+
+	fprintf(stderr, "qux\n");
 
 	return false;
 }
 
 bool push_esc(struct terminal *term, uint8_t ch)
 {
-	if (term->esc.bp == &term->esc.buf[MAX_ESC_LENGTH - 1]) { /* buffer limit */
-		reset_esc(term);
-		return false;
+	long offset;
+
+	fprintf(stderr, "push_esc()\nstate:0x%.2X ch:0x%.2X offset:%ld\n",
+		term->esc.state, ch, term->esc.bp - term->esc.buf);
+
+	if ((term->esc.bp - term->esc.buf + 1) == term->esc.size) { /* buffer limit */
+		if (DEBUG)
+			fprintf(stderr, "escape sequence length > %d, reallocated\n", term->esc.size);
+		offset = term->esc.bp - term->esc.buf;
+		term->esc.buf  = erealloc(term->esc.buf, term->esc.size * 2);
+		term->esc.size *= 2;
+		term->esc.bp   = term->esc.buf + offset;
+		//reset_esc(term);
+		//return false;
 	}
 
+	/* ref: http://www.vt100.net/docs/vt102-ug/appendixd.html */
 	*term->esc.bp++ = ch;
 	if (term->esc.state == STATE_ESC) {
-		if ('0' <= ch && ch <= '~')
+		/* format:
+			ESC  I.......I F
+				 ' '  '/'  '0'  '~'
+			0x1B 0x20-0x2F 0x30-0x7E
+		*/
+		if ('0' <= ch && ch <= '~')        /* final char */
 			return true;
-		else if (SPACE > ch || ch > '/')
-			reset_esc(term);
+		else if (SPACE <= ch && ch <= '/') /* intermediate char */
+			return false;
 	}
 	else if (term->esc.state == STATE_CSI) {
+		/* format:
+			CSI       P.......P I.......I F
+			ESC  '['  '0'  '?'  ' '  '/'  '@'  '~'
+			0x1B 0x5B 0x30-0x3F 0x20-0x2F 0x40-0x7E
+		*/
 		if ('@' <= ch && ch <= '~')
 			return true;
-		else if (SPACE > ch || ch > '?')
-			reset_esc(term);
+		else if (SPACE <= ch && ch <= '?')
+			return false;
 	}
-	else if (term->esc.state == STATE_OSC) {
-		if (is_string_terminator(&term->esc, ch))
+	else {
+		/* format:
+			OSC       I.....I F
+			ESC  ']'          BEL  or ESC  '\'
+			0x1B 0x5D unknown 0x07 or 0x1B 0x5C
+			DCS       I....I  F
+			ESC  'P'          BEL  or ESC  '\'
+			0x1B 0x50 unknown 0x07 or 0x1B 0x5C
+		*/
+		if (is_string_terminator(&term->esc, ch)) {
+			fprintf(stderr, "foo\n");
 			return true;
-		else if ((ch != ESC) && (ch < SPACE || ch >= DEL))
-			reset_esc(term);
+		}
+		else if ((ch == ESC || ch == CR || ch == LF || ch == BS || ch == HT)
+			|| (SPACE <= ch && ch <= '~')) {
+			fprintf(stderr, "bar\n");
+			return false;
+		}
 	}
-	else if (term->esc.state == STATE_DCS) {
-		if (is_string_terminator(&term->esc, ch))
-			return true;
-		else if ((ch != ESC) && (ch < SPACE || ch >= DEL))
-			reset_esc(term);
-	}
+
+
+ 	/* invalid sequence */
+	reset_esc(term);
 	return false;
 }
 
@@ -273,21 +366,13 @@ void reset_charset(struct terminal *term)
 	term->charset.is_valid = true;
 }
 
-void redraw(struct terminal *term)
-{
-	int i;
-
-	for (i = 0; i < term->lines; i++)
-		term->line_dirty[i] = true;
-}
-
 void reset(struct terminal *term)
 {
 	int i, j;
 
-	term->mode = RESET;
-	term->mode |= (MODE_CURSOR | MODE_AMRIGHT);
-	term->wrap = false;
+	term->mode = MODE_RESET;
+	term->mode |= (MODE_CURSOR | MODE_AMRIGHT | MODE_SIXSCR);
+	term->wrap_occured = false;
 
 	term->scroll.top = 0;
 	term->scroll.bottom = term->lines - 1;
@@ -318,13 +403,12 @@ void reset(struct terminal *term)
 	reset_charset(term);
 }
 
-void swap_resolution(int *a, int *b)
+void redraw(struct terminal *term)
 {
-	int tmp;
+	int i;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	for (i = 0; i < term->lines; i++)
+		term->line_dirty[i] = true;
 }
 
 void term_init(struct terminal *term, int width, int height)
@@ -332,25 +416,28 @@ void term_init(struct terminal *term, int width, int height)
 	int i;
 	uint32_t code, gi;
 
-	term->width = width;
+	term->width  = width;
 	term->height = height;
 
-	term->cols = term->width / CELL_WIDTH;
+	term->cols  = term->width / CELL_WIDTH;
 	term->lines = term->height / CELL_HEIGHT;
 
 	if (DEBUG)
 		fprintf(stderr, "width:%d height:%d cols:%d lines:%d\n",
 			width, height, term->cols, term->lines);
 
-	term->line_dirty = (bool *) ecalloc(sizeof(bool) * term->lines);
-	term->tabstop = (bool *) ecalloc(sizeof(bool) * term->cols);
-	term->cells = (struct cell *) ecalloc(sizeof(struct cell) * term->cols * term->lines);
+	term->line_dirty = (bool *) ecalloc(term->lines, sizeof(bool));
+	term->tabstop    = (bool *) ecalloc(term->cols, sizeof(bool));
+	term->cells      = (struct cell *) ecalloc(term->cols * term->lines, sizeof(struct cell));
+
+	term->esc.buf  = (char *) ecalloc(1, MAX_ESC_SIZE);
+	term->esc.size = MAX_ESC_SIZE;
 
 	/* initialize glyph map */
 	for (code = 0; code < UCS2_CHARS; code++)
 		term->glyph_map[code] = NULL;
 
-	for (gi = 0; gi < sizeof(glyphs) / sizeof(struct static_glyph_t); gi++)
+	for (gi = 0; gi < sizeof(glyphs) / sizeof(struct glyph_t); gi++)
 		term->glyph_map[glyphs[gi].code] = &glyphs[gi];
 
 	if (term->glyph_map[DEFAULT_CHAR] == NULL
@@ -360,7 +447,12 @@ void term_init(struct terminal *term, int width, int height)
 
 	/* initialize drcs */
 	for (i = 0; i < DRCS_CHARSETS; i++)
-		term->drcs[i].chars = NULL;
+		term->drcs[i] = NULL;
+
+	/* initialize sixel */
+	term->sixel_canvas_num = 0;
+	for (i = 0; i < MAX_SIXEL_CANVAS; i++)
+		reset_sixel(&term->sixel_canvas[i]);
 
 	reset(term);
 }
@@ -370,4 +462,5 @@ void term_die(struct terminal *term)
 	free(term->line_dirty);
 	free(term->tabstop);
 	free(term->cells);
+	free(term->esc.buf);
 }
