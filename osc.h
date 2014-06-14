c9ca316d857db06cc36e1642d3afc8611292b1f5
uobikiemukot <uobikiemukot@gmail.com>
2014-06-14 14:25:10 +0900

    replace strtol to hex2num

--- osc.h
+++ osc.h
@@ -1,9 +1,109 @@
 /* See LICENSE for licence details. */
 /* function for osc sequence */
+int32_t parse_color1(char *seq)
+{
+	/*
+	format
+		rgb:r/g/b
+		rgb:rr/gg/bb
+		rgb:rrr/ggg/bbb
+		rgb:rrrr/gggg/bbbb
+	*/
+	int i, length, value;
+	int32_t color;
+	uint32_t rgb[3];
+	struct parm_t parm;
+
+	reset_parm(&parm);
+	parse_arg(seq, &parm, '/', isalnum);
+
+	if (DEBUG)
+		for (i = 0; i < parm.argc; i++)
+			fprintf(stderr, "parm.argv[%d]: %s\n", i, parm.argv[i]);
+
+	if (parm.argc != 3)
+		return -1;
+
+	length = strlen(parm.argv[0]);
+
+	for (i = 0; i < 3; i++) {
+		value = hex2num(parm.argv[i]);
+		if (DEBUG)
+			fprintf(stderr, "value:%d\n", value);
+
+		if (length == 1)      /* r/g/b/ */
+			rgb[i] = bit_mask[8] & (value * 0xFF / 0x0F);
+		else if (length == 2) /* rr/gg/bb */
+			rgb[i] = bit_mask[8] & value;
+		else if (length == 3) /* rrr/ggg/bbb */
+			rgb[i] = bit_mask[8] & (value * 0xFF / 0xFFF);
+		else if (length == 4) /* rrrr/gggg/bbbb */
+			rgb[i] = bit_mask[8] & (value * 0xFF / 0xFFFF);
+		else
+			return -1;
+	}
+
+	color = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
+	if (DEBUG)
+		fprintf(stderr, "color:0x%.6X\n", color);
+
+	return color;
+}
+
+int32_t parse_color2(char *seq)
+{
+	/*
+	format
+		#rgb
+		#rrggbb
+		#rrrgggbbb
+		#rrrrggggbbbb
+	*/
+	int i, length;
+	uint32_t rgb[3];
+	int32_t color;
+	char buf[BUFSIZE];
+
+	length = strlen(seq);
+	memset(buf, '\0', BUFSIZE);
+
+	if (length == 3) {       /* rgb */
+		for (i = 0; i < 3; i++) {
+			strncpy(buf, seq + i, 1);
+			rgb[i] = bit_mask[8] & hex2num(buf) * 0xFF / 0x0F;
+		}
+	}
+	else if (length == 6) {  /* rrggbb */
+		for (i = 0; i < 3; i++) { /* rrggbb */
+			strncpy(buf, seq + i * 2, 2);
+			rgb[i] = bit_mask[8] & hex2num(buf);
+		}
+	}
+	else if (length == 9) {  /* rrrgggbbb */
+		for (i = 0; i < 3; i++) {
+			strncpy(buf, seq + i * 3, 3);
+			rgb[i] = bit_mask[8] & hex2num(buf) * 0xFF / 0xFFF;
+		}
+	}
+	else if (length == 12) { /* rrrrggggbbbb */
+		for (i = 0; i < 3; i++) {
+			strncpy(buf, seq + i * 4, 4);
+			rgb[i] = bit_mask[8] & hex2num(buf) * 0xFF / 0xFFFF;
+		}
+	}
+	else
+		return -1;
+
+	color = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
+	if (DEBUG)
+		fprintf(stderr, "color:0x%.6X\n", color);
+
+	return color;
+}
+
 void set_palette(struct terminal *term, void *arg)
 {
 	/*
-
 	OSC Ps ; Pt ST
 	ref: http://invisible-island.net/xterm/ctlseqs/ctlseqs.html
 	ref: http://ttssh2.sourceforge.jp/manual/ja/about/ctrlseq.html#OSC
@@ -23,137 +123,56 @@ void set_palette(struct terminal *term, void *arg)
 				#rrrrggggbbbb
 					this rgb format is "RGB Device String Specification"
 					see http://xjman.dsl.gr.jp/X11R6/X11/CH06.html
-		PT: c ; ?
+		Pt: c ; ?
 			response rgb color
 				OSC 4 ; c ; rgb:rr/gg/bb ST
+
+	TODO: this function only works in 32bpp mode
 	*/
-	struct parm_t *pt = (struct parm_t *) arg, sub_parm;
-	int i, argc = pt->argc, c, length;
-	char **argv = pt->argv;
-	long val;
+	struct parm_t *pt = (struct parm_t *) arg;
+	int i, argc = pt->argc, index;
+	int32_t color;
 	uint8_t rgb[3];
-	uint32_t color;
+	char **argv = pt->argv;
 	char buf[BUFSIZE];
 
 	if (argc != 3)
 		return;
 
+	index = dec2num(argv[1]);
+	if (index < 0 || index >= COLORS)
+		return;
+
 	if (strncmp(argv[2], "rgb:", 4) == 0) {
-		/*
-		rgb:r/g/b
-		rgb:rr/gg/bb
-		rgb:rrr/ggg/bbb
-		rgb:rrrr/gggg/bbbb
-		*/
-		reset_parm(&sub_parm);
-		parse_arg(argv[2] + 4, &sub_parm, '/', isalnum); /* skip "rgb:" */
-
-		if (DEBUG)
-			for (i = 0; i < sub_parm.argc; i++)
-				fprintf(stderr, "sub_parm.argv[%d]: %s\n", i, sub_parm.argv[i]);
-
-		if (sub_parm.argc != 3)
-			return;
-
-		length = strlen(sub_parm.argv[0]);
-		c = atoi(argv[1]);
-
-		for (i = 0; i < 3; i++) {
-			val = strtol(sub_parm.argv[i], NULL, 16);
-			if (DEBUG)
-				fprintf(stderr, "val:%ld\n", val);
-
-			if (length == 1)
-				rgb[i] = (double) val * 0xFF / 0x0F;
-			else if (length == 2)
-				rgb[i] = val;
-			else if (length == 3)
-				rgb[i] = (double) val * 0xFF / 0xFFF;
-			else if (length == 4)
-				rgb[i] = (double) val * 0xFF / 0xFFFF;
-			else
-				return;
-		}
-
-		color = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
-		if (DEBUG)
-			fprintf(stderr, "color:0x%.6X\n", color);
-
-		if (0 <= c && c < COLORS)
-			term->color_palette[c] = color;
+		if ((color = parse_color1(argv[2] + 4)) != -1) /* skip "rgb:" */
+			term->color_palette[index] = (uint32_t) color;
 	}
 	else if (strncmp(argv[2], "#", 1) == 0) {
-		/*
-		#rgb
-		#rrggbb
-		#rrrgggbbb
-		#rrrrggggbbbb
-		*/
-		c = atoi(argv[1]);
-		length = strlen(argv[2] + 1); /* skip '#' */
-		memset(buf, '\0', BUFSIZE);
-
-		if (length == 3) {
-			for (i = 0; i < 3; i++) {
-				strncpy(buf, argv[2] + 1 + i, 1);
-				rgb[i] = (double) strtol(buf, NULL, 16) * 0xFF / 0x0F;
-			}
-		}
-		else if (length == 6) {
-			for (i = 0; i < 3; i++) {
-				strncpy(buf, argv[2] + 1 + i * 2, 2);
-				rgb[i] = strtol(buf, NULL, 16);
-			}
-		}
-		else if (length == 9) {
-			for (i = 0; i < 3; i++) {
-				strncpy(buf, argv[2] + 1 + i * 3, 3);
-				rgb[i] = (double) strtol(buf, NULL, 16) * 0xFF / 0xFFF;
-			}
-		}
-		else if (length == 12) {
-			for (i = 0; i < 3; i++) {
-				strncpy(buf, argv[2] + 1 + i * 4, 4);
-				rgb[i] = (double) strtol(buf, NULL, 16) * 0xFF / 0xFFFF;
-			}
-		}
-		else
-			return;
-
-		color = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
-		if (DEBUG)
-			fprintf(stderr, "color:0x%.6X\n", color);
-
-		if (0 <= c && c < COLORS)
-			term->color_palette[c] = color;
+		if ((color = parse_color2(argv[2] + 1)) != -1) /* skip "#" */
+			term->color_palette[index] = (uint32_t) color;
 	}
 	else if (strncmp(argv[2], "?", 1) == 0) {
-		/*
-		?
-		*/
-		c = atoi(argv[1]);
+		for (i = 0; i < 3; i++)
+			rgb[i] = bit_mask[8] & (term->color_palette[index] >> (8 * (2 - i)));
 
-		if (0 <= c && c < COLORS) {
-			for (i = 2; i >= 0; i--)
-				rgb[i] = (term->color_palette[c] >> (8 * i)) & 0xFF;
-			snprintf(buf, BUFSIZE, "\033]4;%d;rgb:%.2X/%.2X/%.2X\033\\", c, rgb[0], rgb[1], rgb[2]);
-			ewrite(term->fd, buf, strlen(buf));
-		}
+		snprintf(buf, BUFSIZE, "\033]4;%d;rgb:%.2X/%.2X/%.2X\033\\",
+			index, rgb[0], rgb[1], rgb[2]);
+		ewrite(term->fd, buf, strlen(buf));
 	}
 }
 
 void reset_palette(struct terminal *term, void *arg)
 {
 	/*
-		reset color c
-			OSC 104 ; c ST
-				c: index of color
-				ST: BEL or ESC \
-		reset all color
-			OSC 104 ST
-				ST: BEL or ESC \
+	reset color c
+		OSC 104 ; c ST
+			c: index of color
+			ST: BEL or ESC \
+	reset all color
+		OSC 104 ST
+			ST: BEL or ESC \
 
-		terminfo: oc=\E]104\E\\
+	terminfo: oc=\E]104\E\\
 	*/
 	struct parm_t *pt = (struct parm_t *) arg;
 	int i, argc = pt->argc, c;
@@ -164,7 +183,7 @@ void reset_palette(struct terminal *term, void *arg)
 			term->color_palette[i] = color_list[i];
 	}
 	else if (argc == 2) { /* reset color_palette[c] */
-		c = atoi(argv[1]);
+		c = dec2num(argv[1]);
 		if (0 <= c && c < COLORS)
 			term->color_palette[c] = color_list[c];
 	}
@@ -181,28 +200,28 @@ int isdigit_or_questionmark(int c)
 void glyph_width_report(struct terminal *term, void *arg)
 {
 	/*
-		glyph width report
-			* request *
-			OSC 8900 ; Ps ; Pw ; ? : Pf : Pt ST
-				Ps: reserved
-				Pw: width (0 or 1 or 2)
-				Pfrom: beginning of unicode code point
-				Pto: end of unicode code point
-				ST: BEL(0x07) or ESC(0x1B) BACKSLASH(0x5C)
-        	* answer *
-			OSC 8900 ; Ps ; Pv ; Pw ; Pf : Pt ; Pf : Pt ; ... ST
-				Ps: responce code
-					0: ok (default)
-					1: recognized but not supported
-					2: not recognized
-				Pv: reserved (maybe East Asian Width Version)
-				Pw: width (0 or 1 or 2)
-				Pfrom: beginning of unicode code point
-				Pto: end of unicode code point
-				ST: BEL(0x07) or ESC(0x1B) BACKSLASH(0x5C)
-		ref
-			http://uobikiemukot.github.io/yaft/glyph_width_report.html
-			https://gist.github.com/saitoha/8767268
+	glyph width report
+		* request *
+		OSC 8900 ; Ps ; Pw ; ? : Pf : Pt ST
+			Ps: reserved
+			Pw: width (0 or 1 or 2)
+			Pfrom: beginning of unicode code point
+			Pto: end of unicode code point
+			ST: BEL(0x07) or ESC(0x1B) BACKSLASH(0x5C)
+		* answer *
+		OSC 8900 ; Ps ; Pv ; Pw ; Pf : Pt ; Pf : Pt ; ... ST
+			Ps: responce code
+				0: ok (default)
+				1: recognized but not supported
+				2: not recognized
+			Pv: reserved (maybe East Asian Width Version)
+			Pw: width (0 or 1 or 2)
+			Pfrom: beginning of unicode code point
+			Pto: end of unicode code point
+			ST: BEL(0x07) or ESC(0x1B) BACKSLASH(0x5C)
+	ref
+		http://uobikiemukot.github.io/yaft/glyph_width_report.html
+		https://gist.github.com/saitoha/8767268
 	*/
 	struct parm_t *pt = (struct parm_t *) arg, sub_parm;
 	int i, argc = pt->argc, width, from, to, left, right, w, wcw; //reserved
@@ -217,10 +236,10 @@ void glyph_width_report(struct terminal *term, void *arg)
 	if (sub_parm.argc != 3 || *sub_parm.argv[0] != '?')
 		return;
 
-	//reserved = atoi(argv[1]);
-	width = atoi(argv[2]);
-	from = atoi(sub_parm.argv[1]);
-	to = atoi(sub_parm.argv[2]);
+	//reserved = dec2num(argv[1]);
+	width = dec2num(argv[2]);
+	from = dec2num(sub_parm.argv[1]);
+	to = dec2num(sub_parm.argv[2]);
 
 	if ((from < 0 || to >= UCS2_CHARS) /* TODO: change here when implement DRCS */
 		|| (width < 0 || width > 2))
