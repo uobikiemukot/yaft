e4e7a6cccc5aa1d46b73f49ec976833a96f56acd
uobikiemukot <uobikiemukot@gmail.com>
2014-06-15 17:00:18 +0900

    deprecate YAFT="wall" env, add new sixel drawing code

--- linux.h
+++ linux.h
@@ -124,7 +124,7 @@ inline uint32_t color2pixel(struct fb_var_screeninfo *vinfo, uint32_t color)
 void fb_init(struct framebuffer *fb, uint32_t *color_palette)
 {
 	int i;
-	char *path, *env;
+	char *path;
 	struct fb_fix_screeninfo finfo;
 	struct fb_var_screeninfo vinfo;
 
@@ -145,7 +145,7 @@ void fb_init(struct framebuffer *fb, uint32_t *color_palette)
 		ioctl(fb->fd, FBIOPUT_VSCREENINFO, &vinfo);
 	}
 
-	fb->width = vinfo.xres;
+	fb->width  = vinfo.xres;
 	fb->height = vinfo.yres;
 	fb->screen_size = finfo.smem_len;
 	fb->line_length = finfo.line_length;
@@ -165,21 +165,13 @@ void fb_init(struct framebuffer *fb, uint32_t *color_palette)
 	else /* non packed pixel, mono color, grayscale: not implimented */
 		fatal("unsupported framebuffer type");
 
-	for (i = 0; i < COLORS; i++) { /* init color palette */
-		if (fb->bpp == 1)
-			color_palette[i] = i;
-		else
-			color_palette[i] = color2pixel(&vinfo, color_list[i]);
-	}
+	for (i = 0; i < COLORS; i++) /* init color palette */
+		color_palette[i] = (fb->bpp == 1) ? (uint32_t) i: color2pixel(&vinfo, color_list[i]);
 
-	fb->fp = (unsigned char *) emmap(0, fb->screen_size, PROT_WRITE | PROT_READ, MAP_SHARED, fb->fd, 0);
-	fb->buf = (unsigned char *) ecalloc(1, fb->screen_size);
+	fb->fp    = (unsigned char *) emmap(0, fb->screen_size, PROT_WRITE | PROT_READ, MAP_SHARED, fb->fd, 0);
+	fb->buf   = (unsigned char *) ecalloc(1, fb->screen_size);
+	fb->wall  = (WALLPAPER && fb->bpp > 1) ? load_wallpaper(fb): NULL;
 	fb->vinfo = vinfo;
-
-	fb->wall = NULL;
-	if (((env = getenv("YAFT")) != NULL)
-		&& (strstr(env, "wallpaper") != NULL || strstr(env, "wall") != NULL))
-			fb->wall = load_wallpaper(fb);
 }
 
 void fb_die(struct framebuffer *fb)
@@ -189,8 +181,8 @@ void fb_die(struct framebuffer *fb)
 		ioctl(fb->fd, FBIOPUTCMAP, fb->cmap_org); /* not fatal */
 		cmap_die(fb->cmap_org);
 	}
-	free(fb->wall);
 	free(fb->buf);
+	free(fb->wall);
 	emunmap(fb->fp, fb->screen_size);
 	eclose(fb->fd);
 }
@@ -198,48 +190,65 @@ void fb_die(struct framebuffer *fb)
 inline void draw_line(struct framebuffer *fb, struct terminal *term, int line)
 {
 	int pos, size, bit_shift, margin_right;
-	int col, glyph_width_offset, glyph_height_offset;
-	uint32_t pixel;
-	struct color_pair color;
-	struct cell *cp;
-	const struct glyph_t *gp;
+	int col, w, h, src_offset, dst_offset;
+	uint32_t pixel, color = 0;
+	struct color_pair_t color_pair;
+	struct cell_t *cellp;
+	const struct glyph_t *glyphp;
 
 	for (col = term->cols - 1; col >= 0; col--) {
 		margin_right = (term->cols - 1 - col) * CELL_WIDTH;
 
-		/* get cell color and glyph */
-		cp = &term->cells[col + line * term->cols];
-		color = cp->color;
-		gp = cp->gp;
+		/* target cell */
+		cellp = &term->cells[col + line * term->cols];
+
+		/* draw sixel bitmap */
+		if (cellp->has_bitmap) {
+			for (h = 0; h < CELL_HEIGHT; h++) {
+				for (w = 0; w < CELL_WIDTH; w++) {
+					src_offset = BYTES_PER_PIXEL * (h * CELL_WIDTH + w);
+					memcpy(&color, cellp->bitmap + src_offset, BYTES_PER_PIXEL);
+
+					dst_offset = (line * CELL_HEIGHT + h) * fb->line_length + (col * CELL_WIDTH + w) * fb->bpp;
+					pixel = color2pixel(&fb->vinfo, color);
+					memcpy(fb->buf + dst_offset, &pixel, fb->bpp);
+				}
+			}
+			continue;
+		}
+
+		/* get color and glyph */
+		color_pair = cellp->color_pair;
+		glyphp     = cellp->glyphp;
 
 		/* check wide character or not */
-		bit_shift = (cp->width == WIDE) ? CELL_WIDTH: 0;
+		bit_shift = (cellp->width == WIDE) ? CELL_WIDTH: 0;
 
 		/* check cursor positon */
 		if ((term->mode & MODE_CURSOR && line == term->cursor.y)
 			&& (col == term->cursor.x
-			|| (cp->width == WIDE && (col + 1) == term->cursor.x)
-			|| (cp->width == NEXT_TO_WIDE && (col - 1) == term->cursor.x))) {
-			color.fg = DEFAULT_BG;
-			color.bg = (!tty.visible && BACKGROUND_DRAW) ? PASSIVE_CURSOR_COLOR: ACTIVE_CURSOR_COLOR;
+			|| (cellp->width == WIDE && (col + 1) == term->cursor.x)
+			|| (cellp->width == NEXT_TO_WIDE && (col - 1) == term->cursor.x))) {
+			color_pair.fg = DEFAULT_BG;
+			color_pair.bg = (!tty.visible && BACKGROUND_DRAW) ? PASSIVE_CURSOR_COLOR: ACTIVE_CURSOR_COLOR;
 		}
 
-		for (glyph_height_offset = 0; glyph_height_offset < CELL_HEIGHT; glyph_height_offset++) {
+		for (h = 0; h < CELL_HEIGHT; h++) {
 			/* if UNDERLINE attribute on, swap bg/fg */
-			if ((glyph_height_offset == (CELL_HEIGHT - 1)) && (cp->attribute & attr_mask[UNDERLINE]))
-				color.bg = color.fg;
+			if ((h == (CELL_HEIGHT - 1)) && (cellp->attribute & attr_mask[ATTR_UNDERLINE]))
+				color_pair.bg = color_pair.fg;
 
-			for (glyph_width_offset = 0; glyph_width_offset < CELL_WIDTH; glyph_width_offset++) {
-				pos = (term->width - 1 - margin_right - glyph_width_offset) * fb->bpp
-					+ (line * CELL_HEIGHT + glyph_height_offset) * fb->line_length;
+			for (w = 0; w < CELL_WIDTH; w++) {
+				pos = (term->width - 1 - margin_right - w) * fb->bpp
+					+ (line * CELL_HEIGHT + h) * fb->line_length;
 
 				/* set color palette */
-				if (gp->bitmap[glyph_height_offset] & (0x01 << (bit_shift + glyph_width_offset)))
-					pixel = term->color_palette[color.fg];
-				else if (fb->wall && color.bg == DEFAULT_BG) /* wallpaper */
+				if (glyphp->bitmap[h] & (0x01 << (bit_shift + w)))
+					pixel = term->color_palette[color_pair.fg];
+				else if (fb->wall && color_pair.bg == DEFAULT_BG) /* wallpaper */
 					memcpy(&pixel, fb->wall + pos, fb->bpp);
 				else
-					pixel = term->color_palette[color.bg];
+					pixel = term->color_palette[color_pair.bg];
 
 				/* update copy buffer only */
 				memcpy(fb->buf + pos, &pixel, fb->bpp);
@@ -262,51 +271,9 @@ inline void draw_line(struct framebuffer *fb, struct terminal *term, int line)
 	term->line_dirty[line] = ((term->mode & MODE_CURSOR) && term->cursor.y == line) ? true: false;
 }
 
-inline void draw_sixel(struct framebuffer *fb, struct sixel_canvas_t *sc)
-{
-	int h, w, fb_pos, sc_pos;
-	uint32_t color, similar;
-	struct point_t offset;
-
-	offset.x = sc->ref_cell.x * CELL_WIDTH;
-	offset.y = sc->ref_cell.y * CELL_HEIGHT;
-
-	if (DEBUG)
-		fprintf(stderr, "offset(%d, %d) width:%d height:%d\n",
-			offset.x, offset.y, sc->width, sc->height);
-
-	for (h = 0; h < sc->height; h++) {
-		if ((offset.y + h) >= fb->height)
-			break;
-		for (w = 0; w < sc->width; w++) {
-			if ((offset.x + w) >= fb->width)
-				break;
-
-			sc_pos  = w * sizeof(uint32_t) + h * sc->line_length;
-			fb_pos  = (offset.x + w) * fb->bpp + (offset.y + h) * fb->line_length;
-
-			memcpy(&color, sc->data + sc_pos, sizeof(uint32_t));
-			similar = color2pixel(&fb->vinfo, color);
-
-			memcpy(fb->buf + fb_pos, &similar, fb->bpp);
-		}
-		fb_pos = offset.x * fb->bpp + (offset.y + h) * fb->line_length;
-		memcpy(fb->fp + fb_pos, fb->buf + fb_pos, sc->width * fb->bpp);
-	}
-}
-
 void refresh(struct framebuffer *fb, struct terminal *term)
 {
-	int i, line;
-	struct sixel_canvas_t *sc;
-
-	if (term->esc.state == STATE_RESET) {
-	for (i = 0; i < MAX_SIXEL_CANVAS; i++) {
-		sc = &term->sixel_canvas[i];
-		if (sc->data != NULL)
-			draw_sixel(fb, sc);
-	}
-	}
+	int line;
 
 	if (term->mode & MODE_CURSOR)
 		term->line_dirty[term->cursor.y] = true;
