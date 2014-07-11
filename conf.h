3f4e4b9349b0476125dad168d257af2f1ff5c424
uobikiemukot <uobikiemukot@gmail.com>
2014-07-11 18:47:56 +0900

    remove LAZY_DRAW and add preprocessor code

--- conf.h
+++ conf.h
@@ -1,12 +1,5 @@
 /* See LICENSE for licence details. */
 
-/* framubuffer device, shell command
-	are defined in fb/{linux.h,freebsd.h,netbsd.h}
-	please check include file in yaft.c */
-
-/* TERM value */
-const char *term_name = "yaft-256color";
-
 /* color: index number of color_palette[] (see color.h) */
 enum {
 	DEFAULT_FG           = 7,
@@ -19,10 +12,32 @@ enum {
 enum {
 	DEBUG            = false,  /* write dump of input to stdout, debug message to stderr */
 	TABSTOP          = 8,      /* hardware tabstop */
-	LAZY_DRAW        = false,  /* don't draw when input data size is larger than BUFSIZE */
-	BACKGROUND_DRAW  = false,   /* always draw even if vt is not active */
-	WALLPAPER        = false,   /* copy framebuffer before startup, and use it as wallpaper */
-	SUBSTITUTE_HALF  = 0x0020, /* used for missing glyph(single width): U+FFFD (REPLACEMENT CHARACTER)) */
+	BACKGROUND_DRAW  = false,  /* always draw even if vt is not active */
+	WALLPAPER        = false,  /* copy framebuffer before startup, and use it as wallpaper */
+	SUBSTITUTE_HALF  = 0x0020, /* used for missing glyph(single width): U+0020 (SPACE) */
 	SUBSTITUTE_WIDE  = 0x3013, /* used for missing glyph(double width): U+3013 (GETA MARK) */
-	REPLACEMENT_CHAR = 0x0020, /* used for malformed UTF-8 sequence   : U+FFFD (REPLACEMENT CHARACTER)  */
+	REPLACEMENT_CHAR = 0x0020, /* used for malformed UTF-8 sequence   : U+0020 (SPACE)  */
 };
+
+/* TERM value */
+const char *term_name = "yaft-256color";
+
+/* framubuffer device */
+#if defined(__linux__)
+	const char *fb_path = "/dev/fb0";
+#elif defined(__FreeBSD__)
+	const char *fb_path = "/dev/tty";
+#elif defined(__NetBSD__)
+	const char *fb_path = "/dev/ttyE0";
+#elif defined(__OpenBSD__)
+	const char *fb_path = "/dev/ttyC0";
+#endif
+//const char *fb_path = "/dev/graphics/fb0"; /* for Android */
+
+/* shell */
+#if defined(__linux__)
+	const char *shell_cmd = "/bin/bash";
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+	const char *shell_cmd = "/bin/csh";
+#endif
+//const char *shell_cmd = "/system/bin/sh"; /* for Android */
