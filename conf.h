c4cec6babec1607e24de57dd10fea6ad2f6761f6
uobikiemukot <uobikiemukot@gmail.com>
2014-07-13 20:39:54 +0900

    remove WALLPAPER option

--- conf.h
+++ conf.h
@@ -13,7 +13,6 @@ enum {
 	DEBUG            = false,  /* write dump of input to stdout, debug message to stderr */
 	TABSTOP          = 8,      /* hardware tabstop */
 	BACKGROUND_DRAW  = false,  /* always draw even if vt is not active */
-	WALLPAPER        = false,  /* copy framebuffer before startup, and use it as wallpaper */
 	SUBSTITUTE_HALF  = 0x0020, /* used for missing glyph (single width): U+0020 (SPACE) */
 	SUBSTITUTE_WIDE  = 0x3000, /* used for missing glyph (double width): U+3000 (IDEOGRAPHIC SPACE) */
 	REPLACEMENT_CHAR = 0x003F, /* used for malformed UTF-8 sequence    : U+003F (QUESTION MARK)  */
