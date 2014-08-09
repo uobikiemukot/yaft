1298831593bf710d20c5d9e6dfaa4125a2a3025d
uobikiemukot <uobikiemukot@gmail.com>
2014-08-09 21:00:46 +0900

    add LAZY_DRAW

--- conf.h
+++ conf.h
@@ -1,5 +1,4 @@
 /* See LICENSE for licence details. */
-
 /* color: index number of color_palette[] (see color.h) */
 enum {
 	DEFAULT_FG           = 7,
@@ -12,7 +11,9 @@ enum {
 enum {
 	DEBUG            = false,  /* write dump of input to stdout, debug message to stderr */
 	TABSTOP          = 8,      /* hardware tabstop */
+	LAZY_DRAW        = true,   /* don't draw when input data size is larger than BUFSIZE */
 	BACKGROUND_DRAW  = false,  /* always draw even if vt is not active */
+	//WALLPAPER        = true,  /* copy framebuffer before startup, and use it as wallpaper */
 	SUBSTITUTE_HALF  = 0x0020, /* used for missing glyph (single width): U+0020 (SPACE) */
 	SUBSTITUTE_WIDE  = 0x3000, /* used for missing glyph (double width): U+3000 (IDEOGRAPHIC SPACE) */
 	REPLACEMENT_CHAR = 0x003F, /* used for malformed UTF-8 sequence    : U+003F (QUESTION MARK)  */
@@ -33,7 +34,7 @@ const char *term_name = "yaft-256color";
 #endif
 //const char *fb_path = "/dev/graphics/fb0"; /* for Android */
 
-/* shell */
+/* shell: refer SHELL environment at first */
 #if defined(__linux__) || defined(__MACH__)
 	const char *shell_cmd = "/bin/bash";
 #elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
