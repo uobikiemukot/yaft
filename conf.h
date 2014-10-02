4c5b3efe9163782a1d2bc5ae07c009a0cc5045fd
uobikiemukot <uobikiemukot@gmail.com>
2014-10-02 13:46:16 +0900

    change option name DEBUG -> VERBOSE, add new option VT_CONTROL

--- conf.h
+++ conf.h
@@ -1,4 +1,6 @@
 /* See LICENSE for licence details. */
+/* conf.h: define custom variables */
+
 /* color: index number of color_palette[] (see color.h) */
 enum {
 	DEFAULT_FG           = 7,
@@ -9,11 +11,11 @@ enum {
 
 /* misc */
 enum {
-	DEBUG            = false,  /* write dump of input to stdout, debug message to stderr */
+	VERBOSE          = false,  /* write dump of input to stdout, debug message to stderr */
 	TABSTOP          = 8,      /* hardware tabstop */
 	LAZY_DRAW        = true,   /* don't draw when input data size is larger than BUFSIZE */
 	BACKGROUND_DRAW  = false,  /* always draw even if vt is not active */
-	//WALLPAPER        = true,  /* copy framebuffer before startup, and use it as wallpaper */
+	VT_CONTROL       = false,
 	SUBSTITUTE_HALF  = 0x0020, /* used for missing glyph (single width): U+0020 (SPACE) */
 	SUBSTITUTE_WIDE  = 0x3000, /* used for missing glyph (double width): U+3000 (IDEOGRAPHIC SPACE) */
 	REPLACEMENT_CHAR = 0x003F, /* used for malformed UTF-8 sequence    : U+003F (QUESTION MARK)  */
@@ -31,13 +33,15 @@ const char *term_name = "yaft-256color";
 	const char *fb_path = "/dev/ttyE0";
 #elif defined(__OpenBSD__)
 	const char *fb_path = "/dev/ttyC0";
+#elif defined(__ANDROID__)
+	const char *fb_path = "/dev/graphics/fb0";
 #endif
-//const char *fb_path = "/dev/graphics/fb0"; /* for Android */
 
-/* shell: refer SHELL environment at first */
+/* shell: refer SHELL environment variable at first */
 #if defined(__linux__) || defined(__MACH__)
 	const char *shell_cmd = "/bin/bash";
 #elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
 	const char *shell_cmd = "/bin/csh";
+#elif defined(__ANDROID__)
+	const char *shell_cmd = "/system/bin/sh";
 #endif
-//const char *shell_cmd = "/system/bin/sh"; /* for Android */
