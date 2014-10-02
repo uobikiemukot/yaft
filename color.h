d1a4b8c702f5c25731bb3221e87b7d17879f3c10
uobikiemukot <uobikiemukot@gmail.com>
2014-10-02 13:44:16 +0900

    add enum (COLORS), add ref url of xterm 256color

--- color.h
+++ color.h
@@ -3,9 +3,17 @@
 	Standard VGA colors
 	http://en.wikipedia.org/wiki/ANSI_escape_code
 
+	xterm 256color
+	http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
+	http://pln.jonas.me/xterm-colors
+
 	byte order: (MSB) RR GG BB (LSB)
 */
-const uint32_t color_list[256] = {
+enum {
+	COLORS = 256, /* number of color palette */
+};
+
+const uint32_t color_list[COLORS] = {
 	/* system color: 16 */
 	0x000000, 0xAA0000, 0x00AA00, 0xAA5500, 0x0000AA, 0xAA00AA, 0x00AAAA, 0xAAAAAA/* 0xAAAAAA */,
 	0x555555, 0xFF5555, 0x55FF55, 0xFFFF55, 0x5555FF, 0xFF55FF, 0x55FFFF, 0xDFDFDF/* 0xFFFFFF */,
