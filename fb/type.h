6f67b143b53fbc0013c35c0e739a88c43d0f01e2
uobikiemukot <uobikiemukot@gmail.com>
2014-10-10 07:54:29 +0900

    remove unused code

--- fb/type.h
+++ fb/type.h
@@ -4,8 +4,6 @@
     #include <linux/vt.h>
     #include <linux/kd.h>
     typedef struct fb_cmap cmap_t;
-    //typedef __u32          cmap_length_t;
-    //typedef __u16          cmap_color_t;
     #include "linux.h"
 #elif defined(__FreeBSD__)
 	/* XXX: _XOPEN_SOURCE >= 600 invalidates __BSD_VISIBLE
@@ -20,9 +18,6 @@
     #include <sys/kbio.h>
     #include <sys/types.h>
     typedef struct fbcmap cmap_t;
-    //typedef video_color_palette_t cmap_t;
-    //typedef int                   cmap_length_t;
-    //typedef u_char                cmap_color_t;
     #include "freebsd.h"
 #elif defined(__NetBSD__) || defined(__OpenBSD__)
     typedef unsigned char   unchar;
@@ -35,8 +30,6 @@
     #include <dev/wscons/wsconsio.h>
     #include <dev/wscons/wsksymdef.h>
     typedef struct wsdisplay_cmap cmap_t;
-    //typedef int                   cmap_length_t;
-    //typedef u_char                cmap_color_t;
 #elif defined(__NetBSD__)
     #include "netbsd.h"
 #elif defined(__OpenBSD__)
