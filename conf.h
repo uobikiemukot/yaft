30519c4e267808bbbe5f6e34d9a5b2da8a3db901
uobikiemukot <uobikiemukot@gmail.com>
2015-07-22 18:35:55 +0900

    change default value of VT_CONTROL, add comment

--- conf.h
+++ conf.h
@@ -15,7 +15,7 @@ enum {
 	TABSTOP          = 8,      /* hardware tabstop */
 	LAZY_DRAW        = true,   /* don't draw when input data size is larger than BUFSIZE */
 	BACKGROUND_DRAW  = false,  /* always draw even if vt is not active */
-	VT_CONTROL       = false,
+	VT_CONTROL       = true,   /* handle vt switching */
 	SUBSTITUTE_HALF  = 0x0020, /* used for missing glyph (single width): U+0020 (SPACE) */
 	SUBSTITUTE_WIDE  = 0x3000, /* used for missing glyph (double width): U+3000 (IDEOGRAPHIC SPACE) */
 	REPLACEMENT_CHAR = 0x003F, /* used for malformed UTF-8 sequence    : U+003F (QUESTION MARK)  */
