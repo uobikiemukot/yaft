4bd08ab2cbdd01420ce76f0a0367e682f606d76b
uobikiemukot <uobikiemukot@gmail.com>
2015-07-31 15:28:23 +0900

    rewrite yaftx (apply changes of yaft v0.2.9)

--- x/x.h
+++ x/x.h
@@ -3,58 +3,57 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
-#include <X11/Xlocale.h>
 
 #define XK_NO_MOD UINT_MAX
 
-enum { /* default terminal size */
-    TERM_WIDTH  = 640,
-    TERM_HEIGHT = 384,
+enum { /* default terminal size 80x24 (8x16 dot font) */
+	TERM_WIDTH  = 640,
+	TERM_HEIGHT = 384,
 };
 
-struct keydef {
-	KeySym k;
+struct keymap_t {
+	KeySym keysym;
 	unsigned int mask;
-	char s[BUFSIZE];
+	char str[BUFSIZE];
 };
 
-const struct keydef key[] = {
-    { XK_BackSpace, XK_NO_MOD, "\177"    },
-    { XK_Up,        Mod1Mask,  "\033\033[A"},
-    { XK_Up,        XK_NO_MOD, "\033[A"  },
-    { XK_Down,      Mod1Mask,  "\033\033[B"},
-    { XK_Down,      XK_NO_MOD, "\033[B"  },
-    { XK_Right,     Mod1Mask,  "\033\033[C"},
-    { XK_Right,     XK_NO_MOD, "\033[C"  },
-    { XK_Left,      Mod1Mask,  "\033\033[D"},
-    { XK_Left,      XK_NO_MOD, "\033[D"  },
-    { XK_Begin,     XK_NO_MOD, "\033[G"  },
-    { XK_Home,      XK_NO_MOD, "\033[1~" },
-    { XK_Insert,    XK_NO_MOD, "\033[2~" },
-    { XK_Delete,    XK_NO_MOD, "\033[3~" },
-    { XK_End,       XK_NO_MOD, "\033[4~" },
-    { XK_Prior,     XK_NO_MOD, "\033[5~" },
-    { XK_Next,      XK_NO_MOD, "\033[6~" },
-    { XK_F1,        XK_NO_MOD, "\033[[A" },
-    { XK_F2,        XK_NO_MOD, "\033[[B" },
-    { XK_F3,        XK_NO_MOD, "\033[[C" },
-    { XK_F4,        XK_NO_MOD, "\033[[D" },
-    { XK_F5,        XK_NO_MOD, "\033[[E" },
-    { XK_F6,        XK_NO_MOD, "\033[17~"},
-    { XK_F7,        XK_NO_MOD, "\033[18~"},
-    { XK_F8,        XK_NO_MOD, "\033[19~"},
-    { XK_F9,        XK_NO_MOD, "\033[20~"},
-    { XK_F10,       XK_NO_MOD, "\033[21~"},
-    { XK_F11,       XK_NO_MOD, "\033[23~"},
-    { XK_F12,       XK_NO_MOD, "\033[24~"},
-    { XK_F13,       XK_NO_MOD, "\033[25~"},
-    { XK_F14,       XK_NO_MOD, "\033[26~"},
-    { XK_F15,       XK_NO_MOD, "\033[28~"},
-    { XK_F16,       XK_NO_MOD, "\033[29~"},
-    { XK_F17,       XK_NO_MOD, "\033[31~"},
-    { XK_F18,       XK_NO_MOD, "\033[32~"},
-    { XK_F19,       XK_NO_MOD, "\033[33~"},
-    { XK_F20,       XK_NO_MOD, "\033[34~"},
+const struct keymap_t keymap[] = {
+	{XK_BackSpace, XK_NO_MOD, "\177"      },
+	{XK_Up,        Mod1Mask,  "\033\033[A"},
+	{XK_Up,        XK_NO_MOD, "\033[A"    },
+	{XK_Down,      Mod1Mask,  "\033\033[B"},
+	{XK_Down,      XK_NO_MOD, "\033[B"    },
+	{XK_Right,     Mod1Mask,  "\033\033[C"},
+	{XK_Right,     XK_NO_MOD, "\033[C"    },
+	{XK_Left,      Mod1Mask,  "\033\033[D"},
+	{XK_Left,      XK_NO_MOD, "\033[D"    },
+	{XK_Begin,     XK_NO_MOD, "\033[G"    },
+	{XK_Home,      XK_NO_MOD, "\033[1~"   },
+	{XK_Insert,    XK_NO_MOD, "\033[2~"   },
+	{XK_Delete,    XK_NO_MOD, "\033[3~"   },
+	{XK_End,       XK_NO_MOD, "\033[4~"   },
+	{XK_Prior,     XK_NO_MOD, "\033[5~"   },
+	{XK_Next,      XK_NO_MOD, "\033[6~"   },
+	{XK_F1,        XK_NO_MOD, "\033[[A"   },
+	{XK_F2,        XK_NO_MOD, "\033[[B"   },
+	{XK_F3,        XK_NO_MOD, "\033[[C"   },
+	{XK_F4,        XK_NO_MOD, "\033[[D"   },
+	{XK_F5,        XK_NO_MOD, "\033[[E"   },
+	{XK_F6,        XK_NO_MOD, "\033[17~"  },
+	{XK_F7,        XK_NO_MOD, "\033[18~"  },
+	{XK_F8,        XK_NO_MOD, "\033[19~"  },
+	{XK_F9,        XK_NO_MOD, "\033[20~"  },
+	{XK_F10,       XK_NO_MOD, "\033[21~"  },
+	{XK_F11,       XK_NO_MOD, "\033[23~"  },
+	{XK_F12,       XK_NO_MOD, "\033[24~"  },
+	{XK_F13,       XK_NO_MOD, "\033[25~"  },
+	{XK_F14,       XK_NO_MOD, "\033[26~"  },
+	{XK_F15,       XK_NO_MOD, "\033[28~"  },
+	{XK_F16,       XK_NO_MOD, "\033[29~"  },
+	{XK_F17,       XK_NO_MOD, "\033[31~"  },
+	{XK_F18,       XK_NO_MOD, "\033[32~"  },
+	{XK_F19,       XK_NO_MOD, "\033[33~"  },
+	{XK_F20,       XK_NO_MOD, "\033[34~"  },
 };
 
 /* not assigned:
@@ -62,37 +61,57 @@ const struct keydef key[] = {
 */
 
 struct xwindow_t {
-    Display *display;
-    Window window;
-    Pixmap pixbuf;
-    GC gc;
+	Display *display;
+	Window window;
+	Pixmap pixbuf;
+	Colormap cmap;
+	GC gc;
 	int width, height;
-    int screen;
+	int screen;
+	unsigned long color_palette[COLORS];
 };
 
-void xw_init(struct xwindow_t *xw)
+unsigned long color2pixel(struct xwindow_t *xw, uint32_t color)
+{
+	XColor xc;
+
+	xc.red   = ((color >> 16) & bit_mask[8]) << 8;
+	xc.green = ((color >>  8) & bit_mask[8]) << 8;
+	xc.blue  = ((color >>  0) & bit_mask[8]) << 8;
+
+	XAllocColor(xw->display, xw->cmap, &xc);
+	return xc.pixel;
+}
+
+bool xw_init(struct xwindow_t *xw)
 {
 	XTextProperty xtext = {.value = (unsigned char *) "yaftx",
 		.encoding = XA_STRING, .format = 8, .nitems = 5};
 
-	if ((xw->display = XOpenDisplay(NULL)) == NULL)
-		logging(ERROR, "XOpenDisplay failed\n");
+	if ((xw->display = XOpenDisplay(NULL)) == NULL) {
+		logging(ERROR, "XOpenDisplay() failed\n");
+		return false;
+	}
 
 	if (!XSupportsLocale())
-		logging(ERROR, "X does not support locale\n");
+		logging(WARN, "X does not support locale\n");
 
 	if (XSetLocaleModifiers("") == NULL)
-		logging(ERROR, "cannot set locale modifiers\n");
+		logging(WARN, "cannot set locale modifiers\n");
+
+	xw->cmap = DefaultColormap(xw->display, xw->screen);
+	for (int i = 0; i < COLORS; i++)
+		xw->color_palette[i] = color2pixel(xw, color_list[i]);
 
 	xw->screen = DefaultScreen(xw->display);
 	xw->window = XCreateSimpleWindow(xw->display, DefaultRootWindow(xw->display),
-		0, 0, TERM_WIDTH, TERM_HEIGHT, 0, color_list[DEFAULT_FG], color_list[DEFAULT_BG]);
-	XSetWMProperties(xw->display, xw->window, &xtext, NULL, NULL, 0, NULL, NULL, NULL);
+		0, 0, TERM_WIDTH, TERM_HEIGHT, 0, xw->color_palette[DEFAULT_FG], xw->color_palette[DEFAULT_BG]);
+	XSetWMProperties(xw->display, xw->window, &xtext, NULL, NULL, 0, NULL, NULL, NULL); /* return void */
 
 	xw->gc = XCreateGC(xw->display, xw->window, 0, NULL);
 	XSetGraphicsExposures(xw->display, xw->gc, False);
 
-	xw->width = DisplayWidth(xw->display, xw->screen);
+	xw->width  = DisplayWidth(xw->display, xw->screen);
 	xw->height = DisplayHeight(xw->display, xw->screen);
 	xw->pixbuf = XCreatePixmap(xw->display, xw->window,
 		xw->width, xw->height, XDefaultDepth(xw->display, xw->screen));
@@ -101,6 +120,8 @@ void xw_init(struct xwindow_t *xw)
 		ExposureMask | KeyPressMask | StructureNotifyMask);
 
 	XMapWindow(xw->display, xw->window);
+
+	return true;
 }
 
 void xw_die(struct xwindow_t *xw)
@@ -121,7 +142,7 @@ static inline void draw_sixel(struct xwindow_t *xw, int line, int col, struct ce
 			memcpy(&color, cellp->pixmap + BYTES_PER_PIXEL * (h * CELL_WIDTH + w), BYTES_PER_PIXEL);
 
 			if (color_list[DEFAULT_BG] != color) {
-				XSetForeground(xw->display, xw->gc, color);
+				XSetForeground(xw->display, xw->gc, color2pixel(xw, color));
 				XDrawPoint(xw->display, xw->pixbuf, xw->gc,
 					col * CELL_WIDTH + w, line * CELL_HEIGHT + h);
 			}
@@ -138,7 +159,7 @@ static inline void draw_line(struct xwindow_t *xw, struct terminal_t *term, int 
 	const struct glyph_t *glyphp;
 
 	/* at first, fill all pixels of line in backgournd color */
-	XSetForeground(xw->display, xw->gc, color_list[DEFAULT_BG]);
+	XSetForeground(xw->display, xw->gc, xw->color_palette[DEFAULT_BG]);
 	XFillRectangle(xw->display, xw->pixbuf, xw->gc, 0, line * CELL_HEIGHT, term->width, CELL_HEIGHT);
 
 	for (col = term->cols - 1; col >= 0; col--) {
@@ -178,9 +199,9 @@ static inline void draw_line(struct xwindow_t *xw, struct terminal_t *term, int 
 			for (w = 0; w < CELL_WIDTH; w++) {
 				/* set color palette */
 				if (glyphp->bitmap[h] & (0x01 << (bdf_padding + w)))
-					XSetForeground(xw->display, xw->gc, color_list[color_pair.fg]);
+					XSetForeground(xw->display, xw->gc, xw->color_palette[color_pair.fg]);
 				else if (color_pair.bg != DEFAULT_BG)
-					XSetForeground(xw->display, xw->gc, color_list[color_pair.bg]);
+					XSetForeground(xw->display, xw->gc, xw->color_palette[color_pair.bg]);
 				else /* already draw */
 					continue;
 
--- x/yaftx.c
+++ x/yaftx.c
@@ -14,28 +14,25 @@ void sig_handler(int signo)
 {
 	extern volatile sig_atomic_t child_alive;
 
-	if (signo == SIGCHLD)
+	if (signo == SIGCHLD) {
 		child_alive = false;
+		wait(NULL);
+	}
 }
 
-void sig_set()
+bool sig_set(int signo, void (*handler)(int signo), int flags)
 {
 	struct sigaction sigact;
 
 	memset(&sigact, 0, sizeof(struct sigaction));
-	sigact.sa_handler = sig_handler;
-	sigact.sa_flags   = SA_RESTART;
-	esigaction(SIGCHLD, &sigact, NULL);
-}
+	sigact.sa_handler = handler;
+	sigact.sa_flags   = flags;
 
-void sig_reset()
-{
- 	/* no error handling */
-	struct sigaction sigact;
-
-	memset(&sigact, 0, sizeof(struct sigaction));
-	sigact.sa_handler = SIG_DFL;
-	sigaction(SIGCHLD, &sigact, NULL);
+	if (esigaction(signo, &sigact, NULL) == -1) {
+		logging(WARN, "sigaction: signo %d failed\n", signo);
+		return false;
+	}
+	return true;
 }
 
 void check_fds(fd_set *fds, struct timeval *tv, int master)
@@ -47,17 +44,18 @@ void check_fds(fd_set *fds, struct timeval *tv, int master)
 	eselect(master + 1, fds, NULL, NULL, tv);
 }
 
-char *keymap(KeySym k, unsigned int state)
+char *keyremap(KeySym keysym, unsigned int state)
 {
-	int i, length;
+	int length;
+	unsigned int mask;
 
-	length = sizeof(key) / sizeof(key[0]);
+	length = sizeof(keymap) / sizeof(keymap[0]);
 
-	for(i = 0; i < length; i++) {
-		unsigned int mask = key[i].mask;
-		if(key[i].k == k &&
+	for (int i = 0; i < length; i++) {
+		mask = keymap[i].mask;
+		if (keymap[i].keysym == keysym &&
 			((state & mask) == mask || (mask == XK_NO_MOD && !state)))
-			return (char *) key[i].s;
+			return (char *) keymap[i].str;
 	}
 	return NULL;
 }
@@ -69,10 +67,11 @@ void xkeypress(struct xwindow_t *xw, struct terminal_t *term, XEvent *ev)
 	XKeyEvent *e = &ev->xkey;
 	KeySym keysym;
 
+	//size = XmbLookupString(xw->ic, e, buf, BUFSIZE, &keysym, NULL);
 	(void) xw;
 
 	size = XLookupString(e, buf, BUFSIZE, &keysym, NULL);
-	if ((customkey = keymap(keysym, e->state))) {
+	if ((customkey = keyremap(keysym, e->state))) {
 		ewrite(term->fd, customkey, strlen(customkey));
 	} else {
 		if (size == 1 && (e->state & Mod1Mask)) {
@@ -89,9 +88,8 @@ void xresize(struct xwindow_t *xw, struct terminal_t *term, XEvent *ev)
 	XConfigureEvent *e = &ev->xconfigure;
 	struct winsize ws;
 
-	if (DEBUG)
-		fprintf(stderr, "xresize() term.width:%d term.height:%d width:%d height:%d\n",
-			term->width, term->height, e->width, e->height);
+	logging(DEBUG, "xresize() term.width:%d term.height:%d width:%d height:%d\n",
+		term->width, term->height, e->width, e->height);
 
 	(void ) xw; /* unused */
 
@@ -109,7 +107,8 @@ void xresize(struct xwindow_t *xw, struct terminal_t *term, XEvent *ev)
 
 	ws.ws_col = term->cols;
 	ws.ws_row = term->lines;
-	ws.ws_xpixel = ws.ws_ypixel = 0;
+	ws.ws_xpixel = CELL_WIDTH * term->cols;
+	ws.ws_ypixel = CELL_HEIGHT * term->lines;
 	ioctl(term->fd, TIOCSWINSZ, &ws);
 }
 
@@ -118,9 +117,8 @@ void xredraw(struct xwindow_t *xw, struct terminal_t *term, XEvent *ev)
 	XExposeEvent *e = &ev->xexpose;
 	int i, lines, update_from;
 
-	if (DEBUG)
-		fprintf(stderr, "xredraw() count:%d x:%d y:%d width:%d height:%d\n",
-			e->count, e->x, e->y, e->width, e->height);
+	logging(DEBUG, "xredraw() count:%d x:%d y:%d width:%d height:%d\n",
+		e->count, e->x, e->y, e->width, e->height);
 
 	update_from = e->y / CELL_HEIGHT;
 	lines       = my_ceil(e->height, CELL_HEIGHT);
@@ -140,20 +138,29 @@ void (*event_func[LASTEvent])(struct xwindow_t *xw, struct terminal_t *term, XEv
 	[Expose]           = xredraw,
 };
 
-void fork_and_exec(int *master)
+bool fork_and_exec(int *master, int lines, int cols)
 {
+	extern const char *shell_cmd; /* defined in conf.h */
 	char *shell_env;
 	pid_t pid;
+	struct winsize ws = {.ws_row = lines, .ws_col = cols,
+		/* XXX: this variables are UNUSED (man tty_ioctl),
+			but useful for calculating terminal cell size */
+		.ws_ypixel = CELL_HEIGHT * lines, .ws_xpixel = CELL_WIDTH * cols};
 
-	pid = eforkpty(master, NULL, NULL, NULL);
-
-    if (pid == 0) { /* child */
+	pid = eforkpty(master, NULL, NULL, &ws);
+	if (pid < 0)
+		return false;
+	else if (pid == 0) { /* child */
 		esetenv("TERM", term_name, 1);
 		if ((shell_env = getenv("SHELL")) != NULL)
-			eexecvp(shell_env, (const char *[]){shell_env, NULL});
+			eexecl(shell_env);
 		else
-			eexecvp(shell_cmd, (const char *[]){shell_cmd, NULL});
-    }
+			eexecl(shell_cmd);
+		/* never reach here */
+		exit(EXIT_FAILURE);
+	}
+	return true;
 }
 
 int main()
@@ -169,13 +176,27 @@ int main()
 	extern volatile sig_atomic_t child_alive;
 
 	/* init */
-	setlocale(LC_ALL, "");
-	sig_set();
-	xw_init(&xw);
-	term_init(&term, xw.width, xw.height);
+	if (!setlocale(LC_ALL, ""))
+		logging(WARN, "setlocale falied\n");
+
+	if (!sig_set(SIGCHLD, sig_handler, SA_RESTART))
+		logging(ERROR, "signal initialize failed\n");
+
+	if (!xw_init(&xw)) {
+		logging(FATAL, "xwindow initialize failed\n");
+		goto xw_init_failed;
+	}
+
+	if (!term_init(&term, xw.width, xw.height)) {
+		logging(FATAL, "terminal initialize failed\n");
+		goto term_init_failed;
+	}
 
 	/* fork and exec shell */
-	fork_and_exec(&term.fd);
+	if (!fork_and_exec(&term.fd, term.lines, term.cols)) {
+		logging(FATAL, "forkpty failed\n");
+		goto fork_failed;
+	}
 	child_alive = true;
 
 	/* initial terminal size defined in x.h */
@@ -208,7 +229,13 @@ int main()
 	/* die */
 	term_die(&term);
 	xw_die(&xw);
-	sig_reset();
-
+	sig_set(SIGCHLD, SIG_DFL, 0);
 	return EXIT_SUCCESS;
+
+fork_failed:
+	term_die(&term);
+term_init_failed:
+	xw_die(&xw);
+xw_init_failed:
+	return EXIT_FAILURE;
 }
